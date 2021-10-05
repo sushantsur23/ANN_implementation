dc188abefc92a90825d0739594908db4197f7067
Sushant Sur <sushant.sur23@gmail.com>
2021-10-06 00:46:18 +0530

    second

--- src/training.py
+++ src/training.py
@@ -1,14 +1,28 @@
 from src.utils.common import read_config
 import argparse
 from src.utils.data_mgmt import get_data
-
+from src.utils.model import create_model
 
 
 def training(config_path):
     config = read_config(config_path)
     validation_datasize = config["params"]["validation_datasize"]
 
+    LOSS_FUNCTION = config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    NUM_CLASSES = config["params"]["num_classes"]
+
     (x_train,y_train), (x_valid,y_valid), (x_test,y_test) = get_data(validation_datasize)
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+    
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION_SET = (x_valid, y_valid)
+
+    history = model.fit(x_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+
+
+
 
 if __name__=='__main__':
     args= argparse.ArgumentParser()
