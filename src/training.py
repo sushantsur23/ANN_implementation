d869eb2dc8fea34e997d491a0e18ff176f698f5e
Sushant Sur <sushant.sur23@gmail.com>
2021-10-12 00:10:34 +0530

    first

--- src/training.py
+++ src/training.py
@@ -1,33 +1,44 @@
+import os
 from src.utils.common import read_config
-import argparse
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model, save_model
+import argparse
 
 
 def training(config_path):
     config = read_config(config_path)
+
     validation_datasize = config["params"]["validation_datasize"]
+    (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
 
     LOSS_FUNCTION = config["params"]["loss_function"]
     OPTIMIZER = config["params"]["optimizer"]
     METRICS = config["params"]["metrics"]
     NUM_CLASSES = config["params"]["num_classes"]
 
-    (x_train,y_train), (x_valid,y_valid), (x_test,y_test) = get_data(validation_datasize)
     model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
-    
+
     EPOCHS = config["params"]["epochs"]
-    VALIDATION_SET = (x_valid, y_valid)
+    VALIDATION_SET = (X_valid, y_valid)
 
-    history = model.fit(x_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+    history = model.fit(X_train, y_train, epochs=EPOCHS,
+                        validation_data=VALIDATION_SET)
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
+
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
 
 
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
 
-
-if __name__=='__main__':
-    args= argparse.ArgumentParser()
-
-    args.add_argument("--config","-c", default ="config.yaml")
+    args.add_argument("--config", "-c", default="config.yaml")
 
     parsed_args = args.parse_args()
 
